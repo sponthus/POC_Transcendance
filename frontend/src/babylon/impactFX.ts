557435bc8346fb5e5f40b8abfddadc4b7629958a
m-bogey <155486876+m-bogey@users.noreply.github.com>
2025-07-19 00:36:10 +0200

    anime feu crab

--- frontend/src/babylon/impactFX.ts
+++ frontend/src/babylon/impactFX.ts
@@ -123,3 +123,44 @@ export function crabmehamehaFX(scene: Babylon.Scene, position: Babylon.Vector3)
 		particleSystem.dispose();
 	}, 100);
 }
+
+export function spellAvailableFX(scene: Babylon.Scene, position: Babylon.Vector3)
+{
+    const texture = new Babylon.Texture(
+    	"https://www.babylonjs-playground.com/textures/flare.png",
+    	scene);
+
+	const particleSystem = new Babylon.ParticleSystem("spellAvailable", 10, scene);
+
+	particleSystem.particleTexture = texture;
+	particleSystem.emitter = position.clone();
+
+	particleSystem.minEmitBox = new Babylon.Vector3(0, 0, 0);
+	particleSystem.maxEmitBox = new Babylon.Vector3(0, 0, 0);
+
+	particleSystem.color1 = new Babylon.Color4(0, 0.2, 0.9, 0);
+	particleSystem.color2 = new Babylon.Color4(0, 0.2, 0.9, 0);
+	particleSystem.colorDead = new Babylon.Color4(0, 0.2, 0.9, 0);
+
+	// Taille des particules
+    particleSystem.minSize = 0.1;
+    particleSystem.maxSize = 0.3;
+
+	particleSystem.minLifeTime = 0.2;
+	particleSystem.maxLifeTime = 0.4;
+
+	particleSystem.emitRate = 100;
+	particleSystem.direction1 = new Babylon.Vector3(0, 1, 0);
+	//particleSystem.direction2 = new Babylon.Vector3(1, 1, 1);
+
+	particleSystem.minEmitPower = 2;
+	particleSystem.maxEmitPower = 5;
+	particleSystem.updateSpeed = 0.06;
+
+	particleSystem.start();
+
+	setTimeout(() => {
+		particleSystem.stop();
+		particleSystem.dispose();
+	}, 100);
+}
