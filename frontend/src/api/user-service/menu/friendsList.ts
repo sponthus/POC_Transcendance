3c641b1d31530e62a4ef99eaf8924d54d6d6116b
briere <briere.elodie@gmail.com>
2025-08-21 15:33:25 +0200

    try to merge

--- frontend/src/api/user-service/menu/friendsList.ts
+++ frontend/src/api/user-service/menu/friendsList.ts
@@ -4,6 +4,7 @@ type Failure = { ok: false; error: string };
 
 export type FriendsResult = FriendsSuccess | Failure;
 
+//marche mais beaucoup de cas pas gérer (doublon par exemple), faire attention
 export async function   addFriend(username: string): Promise<FriendsResult>
 {
     const token = localStorage.getItem("token");
@@ -22,3 +23,22 @@ export async function   addFriend(username: string): Promise<FriendsResult>
     }
     return { ok: false, error: data.error};
 }
+
+//marche pas 
+/*export async function   getAllFriends(): Promise<FriendsResult>
+{
+    const token = localStorage.getItem("token");
+    if (!token)
+        return {ok: false, error: "No token found"};
+    const res = await fetch('/api/user/menu/friendslist', 
+    {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${token}` },
+    });
+    const data = await res.json();    
+    if (res.ok) 
+    {
+        return { ok: true,  };
+    }
+    return { ok: false, error: data.error};
+}*/
